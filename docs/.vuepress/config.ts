33fd03b374356e43193a212bf55e9e00d8bbeda1
angelyhch <angelyhch@163.com>
2022-10-19 15:33:31 +0800

    主页描述更新

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -18,8 +18,8 @@ export default defineConfig4CustomTheme<VdoingThemeConfig>({
   locales: {
     '/': {
       lang: 'zh-CN',
-      title: "Evan's blog",
-      description: 'web前端技术博客,专注web前端学习与总结。JavaScript,js,ES6,TypeScript,vue,React,python,css3,html5,Node,git,github等技术文章。',
+      title: "汽车焊装驿站",
+      description: '汽车制造焊装专业,相关知识/技术/信息.',
     }
   },
   // base: '/', // 默认'/'。如果你想将你的网站部署到如 https://foo.github.io/bar/，那么 base 应该被设置成 "/bar/",（否则页面将失去样式等文件）
